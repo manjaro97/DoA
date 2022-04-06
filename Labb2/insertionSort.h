f6e8b599bd7e8aca68c3c33648a5066f5ea966f9
manjaro97 <43409726+manjaro97@users.noreply.github.com>
2022-04-06 13:51:50 +0200

    Delete Labb2 directory

--- Labb2/insertionSort.h
+++
@@ -1,9 +0,0 @@
-#ifndef INSERTIONSORT_H
-#define INSERTIONSORT_H
-
-#include <vector>
-
-//Declaring Insertion Sort Algorithm with unsorted data as parameter and returns sorted data
-std::vector<int> InsertionSort(std::vector<int> sortData);
-
-#endif
--- Labb2/selectionSort.h
+++
@@ -1,9 +0,0 @@
-#ifndef SELECTIONSORT_H
-#define SELECTIONSORT_H
-
-#include <vector>
-
-//Declaring Selection Sort Algorithm with unsorted data as parameter and returns sorted data
-std::vector<int> SelectionSort(std::vector<int> sortData);
-
-#endif
